fb03e4b95d6da004b47968b39babdee15b98f3df
yilmale <yilmaz@auburn.edu>
2016-11-20 19:59:45 -0600

    Object algebra

--- src/main/scala-2.12/FeatureModelMain.scala
+++ src/main/scala-2.12/FeatureModelMain.scala
@@ -3,25 +3,41 @@
   */
 
 import algebra._
+import algebra.MechanimComb._
+import AlgebraCore._
+
 
 object FeatureModelMain extends App {
 
-   object F1Algebra extends F1
-   object F2Algebra extends F2
+  object F1Algebra extends F1Generator
+  object F2Algebra extends F2Generator
+  object F3Algebra extends F3Generator
 
-   object F1_F2Algebra extends MechanismMerge[Feature1,Feature2] {
+  object F1_F2Algebra extends MechanismMerge[Feature1,Feature2] {
       val alg1 = F1Algebra
       val alg2 = F2Algebra
       val lifter = LiftF1_F2
-   }
+  }
 
-   val o1:Feature1 = Algebra.exp(F1Algebra)
-   val o2:Feature2 = Algebra.exp(F2Algebra)
+  val o1:Feature1 = ExpAlgebra.exp(F1Algebra)
+  val o2:Feature2 = ExpAlgebra.exp(F2Algebra)
 
-   println("Expression: " + o2.print() + "\nevaluates to: " + o1.eval())
+  println("Expression: " + o2.print() + "\nevaluates to: " + o1.eval())
 
-   val o3 = Algebra.exp(F1_F2Algebra)
+  val o3 = ExpAlgebra.exp(F1_F2Algebra)
 
-   println("Eval: " + o3.eval() + "\nPrint: " + o3.print())
+  println("Eval: " + o3.eval() + "\nPrint: " + o3.print())
+
+  val o4 = ExpAlgebra.exp(merge(LiftF1_F2,F1Algebra,F2Algebra))
+  println("Eval Merge: " + o4.eval() + "\nPrint Merge: " + o4.print())
+
+
+  val step2 = merge(LiftF1F2_F3, merge(LiftF1_F2, F1Algebra, F2Algebra), F3Algebra)
+
+  val o5 = ExpAlgebra.exp(step2)
+  println("Eval Double Merge: " + o5.eval() + "\nPrint Double Merge: " + o5.print() +
+    "\nCount Double Merge: " + o5.count())
+
+
 
 }
--- src/main/scala-2.12/algebra/Algebra.scala
+++ src/main/scala-2.12/algebra/Algebra.scala
@@ -4,6 +4,11 @@ package algebra
   * Created by yilmaz on 11/17/16.
   */
 
+import scala.collection.mutable.HashMap
+import scala.reflect._
+import java.lang.reflect.Method
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.Proxy
 
 trait Feature1 {
   def eval() : Int
@@ -13,13 +18,17 @@ trait Feature2 {
   def print() :String
 }
 
+trait Feature3 {
+  def count() : Int
+}
+
 
 trait MechanismAlg[E] {
   def Lit(x:Int) : E
   def Add(e1:E, e2: E) : E
 }
 
-trait F1 extends MechanismAlg[Feature1] {
+trait F1Generator extends MechanismAlg[Feature1] {
   def Lit(x : Int) : Feature1 = new Feature1 {
     def eval() : Int = x
   }
@@ -31,7 +40,7 @@ trait F1 extends MechanismAlg[Feature1] {
 
 
 
-trait F2 extends MechanismAlg[Feature2] {
+trait F2Generator extends MechanismAlg[Feature2] {
   def Lit(x : Int) : Feature2 = new Feature2 {
     def print() : String = x.toString()
   }
@@ -41,9 +50,26 @@ trait F2 extends MechanismAlg[Feature2] {
   }
 }
 
-trait Lifter[A,B] {
-  def lift(x : A, y : B) : A with B
+trait F3Generator extends MechanismAlg[Feature3] {
+  def Lit(x : Int) : Feature3 = new Feature3 {
+    def count() : Int = x
+  }
+
+  def Add(e1: Feature3, e2: Feature3): Feature3 =  new Feature3 {
+    def count() : Int = {e1.count() + e2.count()}
+  }
 }
+
+
+
+trait Lifter[A,B] {
+  def lift(x : A, y : B ) : A with B
+}
+
+class MkLifter[A,B](f : (A,B) => A with B) extends Lifter[A,B] {
+  def lift(x : A, y : B) : A with B = f(x,y)
+}
+
 
 trait MechanismMerge[A,B] extends MechanismAlg[A with B] {
   val lifter : Lifter[A,B]
@@ -67,7 +93,93 @@ object LiftF1_F2 extends Lifter[Feature1,Feature2] {
   }
 }
 
+object LiftF1_F3 extends Lifter[Feature1,Feature3] {
+  def lift(x : Feature1, y : Feature3) = new Feature1 with Feature3 {
+    def eval() = x.eval()
+    def count() = y.count()
+  }
+}
 
-object Algebra {
+object LiftF1F2_F3 extends Lifter[Feature1 with Feature2,Feature3] {
+  def lift(x : Feature1 with Feature2, y : Feature3) = new Feature1 with Feature2 with Feature3 {
+    def eval() = x.eval()
+    def print() = x.print()
+    def count() = y.count()
+  }
+}
+
+
+
+
+trait MechanismEmpty extends MechanismAlg[Any] {
+  def Lit(x : Int) : Any = new Object()
+  def Add(e1: Any, e2: Any) : Any = new Object()
+}
+
+object MechEmpty extends MechanismEmpty
+
+class LiftDecorate[A](action : A => A) extends Lifter[A,Any] {
+  def lift(x : A, y : Any) = action(x)
+}
+
+trait ExpEmpty extends MechanismAlg[Any] {
+  def Lit(x : Int) : Any = new Object()
+  def Add(e1 : Any, e2 : Any) : Any = new Object ()
+}
+
+object ExpEmpty extends ExpEmpty
+
+trait ExpDecorate[A] extends MechanismMerge[A,Any] {
+  val alg2 = ExpEmpty
+  val lifter = new LiftDecorate(action)
+  def action(x : A) : A
+}
+
+
+
+trait Algebra[F[_]] {
+  def createInstance[A](ih : InvocationHandler)(implicit m : ClassTag[A]) : A = {
+    Proxy.newProxyInstance(m.runtimeClass.getClassLoader, Array(m.runtimeClass),ih).asInstanceOf[A]
+  }
+  // Basic combinators
+  def merge[A,B](lifter : Lifter[A,B], a1 : F[A], a2 : F[B])(implicit m : ClassTag[F[A with B]]) : F[A with B] =
+  createInstance(new InvocationHandler() {
+    def invoke(proxy : Object, method : Method, args : Array[Object]) = {
+      val a = method.invoke(a1,args : _*)
+      val b = method.invoke(a2,args : _*)
+      lifter.lift(a.asInstanceOf[A],b.asInstanceOf[B]).asInstanceOf[Object]
+    }})
+
+  def empty(implicit m : ClassTag[F[Any]]) : F[Any] =
+    createInstance(new InvocationHandler() {
+      def invoke(proxy : Object, method : Method, args : Array[Object]) = new Object()
+    })
+
+  // Derived combinator(s)
+  def delegate[A,B](x : A, y : B)(implicit m : ClassTag[A with B]) : A with B = createInstance(new InvocationHandler() {
+    def invoke(proxy : Object, method : Method, args : Array[Object]) : Object = {
+      try {
+        method.invoke(x, args : _*)
+      } catch {
+        case e : IllegalArgumentException => method.invoke(y, args : _*)
+      }
+    }
+  })
+
+  def combine[A,B](alg1 : F[A], alg2 : F[B])(implicit m1 : ClassTag[F[A with B]], m2 : ClassTag[A with B]) : F[A with B] =
+    merge[A,B](new MkLifter(delegate[A,B] _), alg1, alg2)
+
+  def decorate[A](parent : F[A], action : A => A)(implicit m1 : ClassTag[F[A]], m2 : ClassTag[F[Any]]) : F[A] =
+    merge[A,Any](new LiftDecorate(action),parent,empty)
+}
+
+
+
+
+object MechanimComb extends Algebra[MechanismAlg]
+
+
+
+object ExpAlgebra {
   def exp[E](f: MechanismAlg[E]) : E =  f.Add(f.Lit(5),f.Add(f.Lit(6),f.Lit(6)))
 }
